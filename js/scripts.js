2c6a81987793b9f016c750a1f17c4a4b9dc8b1e7
crossdsection <chirkut.kr@gmail.com>
2018-09-08 14:18:47 +0530

    Added Particle.JS and TypeD.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,109 @@
 function showTab(evt, tabName) {
-        // Declare all variables
-        var i, tabcontent, tablinks;
+  // Declare all variables
+  var i, tabcontent, tablinks;
 
-        // Get all elements with class="tabcontent" and hide them
-        tabcontent = document.getElementsByClassName("modalbox");
-        for (i = 0; i < tabcontent.length; i++) {
-            tabcontent[i].style.display = "none";
-        }
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("modalbox");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
 
-        // Get all elements with class="tablinks" and remove the class "active"
-        tablinks = document.getElementsByClassName("tablinks");
-        for (i = 0; i < tablinks.length; i++) {
-            tablinks[i].className = tablinks[i].className.replace(" active", "");
-        }
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
 
-        // Show the current tab, and add an "active" class to the link that opened the tab
-        document.getElementById(tabName).style.display = "block";
-        if( evt != null ){
-          evt.currentTarget.className += " active";
-        } 
-    }
-
-function closeTab(evt, tabName) {
-    document.getElementById(tabName).style.display = "none";
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(tabName).style.display = "block";
+  if( evt != null ){
+    evt.currentTarget.className += " active";
+  }
 }
 
-showTab(event, 'about');
+function scrollEvent(){
+  console.log( document.body.scrollTop );
+  if( document.body.scrollTop > 0 && document.body.scrollTop < 24 ){
+    document.getElementById( "aboutTab" ).click();
+  } else if( document.body.scrollTop > 24 && document.body.scrollTop < 48 ){
+    document.getElementById( "educationTab" ).click();
+  } else if( document.body.scrollTop > 48 && document.body.scrollTop < 72 ){
+    document.getElementById( "workTab" ).click();
+  } else if( document.body.scrollTop > 72 && document.body.scrollTop < 96 ){
+    document.getElementById( "contactTab" ).click();
+  } else {
+    return false;
+  }
+}
+
+function closeTab(evt, tabName) {
+  document.getElementById(tabName).style.display = "none";
+}
+
+document.getElementById( 'aboutTab' ).click();
+
+/* Typd JS Init */
+var options = {
+  strings: ["My name is <strong>Piyush Kumar</strong> a.k.a crossdsection."],
+  typeSpeed: 40
+};
+
+var typed = new Typed(".element", options);
+
+
+/* Particle JS Init */
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 160, density: { enable: true, value_area: 800 } },
+    color: { value: "#ffffff" },
+    shape: {
+      type: "circle",
+      stroke: { width: 0, color: "#000000" },
+      polygon: { nb_sides: 5 },
+      image: { src: "img/github.svg", width: 100, height: 100 }
+    },
+    opacity: {
+      value: 1,
+      random: true,
+      anim: { enable: true, speed: 1, opacity_min: 0, sync: false }
+    },
+    size: {
+      value: 3,
+      random: true,
+      anim: { enable: false, speed: 4, size_min: 0.3, sync: false }
+    },
+    line_linked: {
+      enable: false,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 1,
+      direction: "none",
+      random: true,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: { enable: false, rotateX: 600, rotateY: 600 }
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: { enable: true, mode: "bubble" },
+      onclick: { enable: true, mode: "repulse" },
+      resize: true
+    },
+    modes: {
+      grab: { distance: 400, line_linked: { opacity: 1 } },
+      bubble: { distance: 250, size: 0, duration: 2, opacity: 0, speed: 3 },
+      repulse: { distance: 400, duration: 0.4 },
+      push: { particles_nb: 4 },
+      remove: { particles_nb: 2 }
+    }
+  },
+  retina_detect: true
+});
