30efd94162a55ac59f7d9e355555cea0e1809fd0
crossdsection <chirkut.kr@gmail.com>
2018-09-09 15:06:15 +0530

    Modified Particle JS settings

--- js/scripts.js
+++ js/scripts.js
@@ -54,13 +54,13 @@ var typed = new Typed(".element", options);
 /* Particle JS Init */
 particlesJS("particles-js", {
   particles: {
-    number: { value: 160, density: { enable: true, value_area: 800 } },
+    number: { value: 280, density: { enable: true, value_area: 1200 } },
     color: { value: "#ffffff" },
     shape: {
-      type: "circle",
-      stroke: { width: 0, color: "#000000" },
-      polygon: { nb_sides: 5 },
-      image: { src: "img/github.svg", width: 100, height: 100 }
+      type: "star",
+      stroke: { width: 0, color: "#ffffff" },
+      polygon: { nb_sides: 10 },
+      image: { src: "img/star.svg", width: 100, height: 100 }
     },
     opacity: {
       value: 1,
@@ -70,7 +70,7 @@ particlesJS("particles-js", {
     size: {
       value: 3,
       random: true,
-      anim: { enable: false, speed: 4, size_min: 0.3, sync: false }
+      anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
     },
     line_linked: {
       enable: false,
@@ -81,7 +81,7 @@ particlesJS("particles-js", {
     },
     move: {
       enable: true,
-      speed: 1,
+      speed: 4,
       direction: "none",
       random: true,
       straight: false,
@@ -93,14 +93,14 @@ particlesJS("particles-js", {
   interactivity: {
     detect_on: "canvas",
     events: {
-      onhover: { enable: true, mode: "bubble" },
-      onclick: { enable: true, mode: "repulse" },
+      onhover: { enable: true, mode: "repulse" },
+      onclick: { enable: true, mode: "push" },
       resize: true
     },
     modes: {
-      grab: { distance: 400, line_linked: { opacity: 1 } },
+      grab: { distance: 200, line_linked: { opacity: 1 } },
       bubble: { distance: 250, size: 0, duration: 2, opacity: 0, speed: 3 },
-      repulse: { distance: 400, duration: 0.4 },
+      repulse: { distance: 200, duration: 0.4 },
       push: { particles_nb: 4 },
       remove: { particles_nb: 2 }
     }
